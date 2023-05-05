55f098cc000303ccb38167de5598903b97eaa40e
Pradeep <ppujari@github.com>
2023-05-04 19:42:57 -0700

    reverted to gz

--- open_clip/tokenizer.py
+++ open_clip/tokenizer.py
@@ -19,7 +19,7 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 @lru_cache()
 def default_bpe():
-    return os.path.join(os.path.dirname(os.path.abspath(__file__)), "bpe_simple_vocab_16e6.txt")
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), "bpe_simple_vocab_16e6.txt.gz")
 
 
 @lru_cache()
